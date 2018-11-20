7fedd14c910ac1aadd4a41e1d3bdeb467d05e301
Derek Smith <dereksmith@Dereks-MacBook-Air.local>
2018-11-19 20:10:26 -0800

    added delete feature

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,22 +2,33 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    value: this.props.value
     // tags: ["tag1", "tag2", "tag3"]
   };
 
   handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
+    this.setState({
+      value: this.state.value + 1,
+      count: this.state.count + 1
+    });
   };
   render() {
     return (
       <React.Fragment>
+        {this.props.children}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={this.handleIncrement}
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
         {/* <ul>
           {this.state.tags.map(tag => (
